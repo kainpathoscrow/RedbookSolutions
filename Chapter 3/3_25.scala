857fafbae805ca59ada41cf24cefded5c8993f4b
kainpathoscrow <maximahatov@nxt.ru>
2020-06-06 00:20:58 +0500

    3_25 fixed, 3_27

--- Chapter 3/3_25.scala
+++ Chapter 3/3_25.scala
@@ -1,6 +1,13 @@
 // Write a function size that counts the number of nodes (leaves and branches) in a tree.
 
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
 def size[A](tree: Tree[A]): Int = tree match {
   case Branch(left, right) => 1 + size(left) + size(right)
   case Leaf(_) => 1
 }
+
+assert(size(Leaf(1)) == 1)
+assert(size(Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))) == 5)
---
+++ Chapter 3/3_27.scala
@@ -0,0 +1,13 @@
+// Write a function depth that returns the maximum path length from the root of a tree to any leaf.
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+def depth(tree: Tree[Int]): Int = tree match {
+  case Branch(left, right) => 1 + depth(left).max(depth(right))
+  case Leaf(_) => 0
+}
+
+assert(depth(Leaf(1)) == 0)
+assert(depth(Branch(Leaf(3), Branch(Leaf(2), Leaf(1)))) == 2)
