1d90bcabebcc94deae437eb10a31905356e60633
kainpathoscrow <maximahatov@nxt.ru>
2020-06-19 20:13:37 +0500

    5_7

--- Chapter 5/5_6.scala
+++ Chapter 5/5_6.scala
@@ -3,4 +3,4 @@
 def headOption[A](as: Stream[A]): Option[A] = as.foldRight(None: Option[A])((cur, _) => Some(cur))
 
 assert(headOption(Stream.empty) == None)
-assert(headOption(1 #:: 2 #:: Stream.empty) == Some(1))
+assert(headOption(1 #:: 2 #:: 3 #:: Stream.empty) == Some(1))
