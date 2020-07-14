0cc446e1108d409805c99632b3c5e6adf0c20f3a
kainpathoscrow <maximahatov@nxt.ru>
2020-07-15 00:53:10 +0500

    11_4

---
+++ Chapter 11/11_4.scala
@@ -0,0 +1,15 @@
+// Implement replicateM.
+
+import cats._
+import cats.implicits._
+
+// Defs
+def sequence[A, M[_]](mas: List[M[A]])(implicit m: Monad[M]): M[List[A]] =
+  mas.foldLeft(m.pure(Nil: List[A]))((acc, cur) => m.flatMap(acc)(accV => m.map(cur)(curV => accV :+ curV)))
+
+// Solution
+def replicateM[A, F[_]: Monad](n: Int, ma: F[A]): F[List[A]] = sequence(List.fill(n)(ma))
+
+// Tests 
+println(replicateM(3, Some(1): Option[Int]))
+assert(replicateM(3, Some(1): Option[Int]) == Some(List(1,1,1)))
