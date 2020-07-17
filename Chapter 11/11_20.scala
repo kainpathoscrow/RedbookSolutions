796accd6394cc0e51a99d355f1d859d4d704a841
kainpathoscrow <maximahatov@nxt.ru>
2020-07-17 20:07:55 +0500

    fix 11_20 with tailrec

--- Chapter 11/11_20.scala
+++ Chapter 11/11_20.scala
@@ -1,22 +1,23 @@
 // Give a monad instance for the following type
 
 import cats._
-
-// Defs
-case class Reader[R, A](run: R => A)
+import cats.data._
+import scala.annotation.tailrec
 
 // Solution
-object Reader {
-  def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
-    def pure[A](a: A): Reader[R,A] = Reader(_ => a)
-    def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] =
-      Reader(r => f(st.run(r)).run(r))
-    
-    def tailRecM[A, B](a: A)(f: A => Reader[R, Either[A, B]]): Reader[R, B] = {
-      flatMap(f(a)){
-        case Left(nextA) => tailRecM(nextA)(f)
-        case Right(b) => pure(b)
-      }
+
+def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
+  def pure[A](a: A): Reader[R,A] = Reader.apply(_ => a)
+  def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] =
+    Reader.apply(r => f(st.run(r)).run(r))
+
+  def tailRecM[A, B](a: A)(f: A => Reader[R, Either[A, B]]): Reader[R, B] = {
+    @tailrec
+    def loop(thisA: A)(r: R): B =
+      f(thisA)(r) match {
+        case Right(b)    => b
+        case Left(nextA) => loop(nextA)(r)
     }
+    Reader.apply(loop(a))
   }
 }
